f02e627440d3d36f9b6ff7ff39c9d5d06e69d80b
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-09-29 10:07:42 +0200

    ElPrimeroprograma4

---
+++ programa3/src/programa3/Programa3.java
@@ -0,0 +1,33 @@
+
+package programa3;
+
+public class Programa3 {
+
+   public static void main(String[] args) {
+        //Incrementos
+        int num1= 5, resultado, modulo,cociente;
+        
+        resultado = num1++;
+        System.out.println("resultado = "+ resultado+"       num1 = "+num1);
+        resultado = ++num1;
+        System.out.println("resultado = "+ resultado+"       num1 = "+num1);
+        resultado+= 3;
+        System.out.println("resultado = "+resultado);
+        modulo = resultado%3;
+        System.out.println("modulo = " +modulo);
+        cociente = resultado/num1;
+        System.out.println("cociente = " +cociente);
+        
+        
+        
+                
+                
+                
+        
+                
+                
+   }
+   
+  
+    
+}
