a117288bb69741c43b18f3128e19ccad53ed9130
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-05 09:19:52 +0200

    ElPrimeroprograma4

--- HOLAMUNDO/src/holamundo/HOLAMUNDO.java
+++ HOLAMUNDO/src/holamundo/HOLAMUNDO.java
@@ -4,7 +4,7 @@ public class HOLAMUNDO {
 
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, Que Tal?");
+        System.out.println("Hola Mundo, Como Estais?");
     }
     
 }
