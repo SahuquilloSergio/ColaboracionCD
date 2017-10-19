ac1df0a6f7877e82d06000a2f417f9ca6b2d962f
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-19 08:56:18 +0200

    Codigo Limpio

---
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,20 @@
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+        
+        float base = 4f,altura = 3f;
+
+        System.out.println("area = "+ (base*altura));
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
