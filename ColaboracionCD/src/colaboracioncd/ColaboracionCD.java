c2d0f7bf1ce41f788fc5d0baef962ba68e05e446
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-20 09:06:53 +0200

    COLABO CD

---
+++ ColaboracionCD/src/colaboracioncd/ColaboracionCD.java
@@ -0,0 +1,13 @@
+package colaboracioncd;
+
+/**
+ *
+ * @author Sergio "Sauky" Sahuquillo
+ */
+public class ColaboracionCD {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Ayuda Pido Cambio de Compañero");
+    }
+    
+}
