539db8166b27dc42e562aade4eed3c3ba47a3d50
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-20 09:00:15 +0200

    Commit 1 ColaboCD

---
+++ COLABOCD/src/colabocd/COLABOCD.java
@@ -0,0 +1,14 @@
+
+package colabocd;
+
+/**
+ *
+ * @author Sergio "Sauky" Sahuquillo
+ */
+public class COLABOCD {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Ayuda Pido Cambio de Compañero");
+    }
+    
+}
