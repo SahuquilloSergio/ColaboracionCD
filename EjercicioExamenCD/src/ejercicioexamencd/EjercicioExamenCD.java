f96eb94e489e4f70056d3505c13e022c53ef4d64
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-20 08:55:55 +0200

    Ejercicio Colabo

---
+++ EjercicioExamenCD/src/ejercicioexamencd/EjercicioExamenCD.java
@@ -0,0 +1,13 @@
+package ejercicioexamencd;
+
+public class EjercicioExamenCD {
+
+    public static void main(String[] args) {
+        System.out.println("Hola ayuda pido cambio de compañero");
+        
+        /*Director: Sergio "Sauky" Sahuquillo
+          Colaborador 1:
+        */
+    }
+    
+}
